c10c7db69b0ca58eedeb9c19b0a5ac8046f17c4b
SilverSol <Silversol3301@gmail.com>
2023-03-16 22:32:18 +0200

    first version of localStorage usage

--- src/Components/FavCities/favCities.jsx
+++ src/Components/FavCities/favCities.jsx
@@ -1,59 +1,80 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect } from "react";
 import { Tab, Tabs } from "@mui/material";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { startCity } from "../../rdx/Features/City/citySlice";
+import { useDispatch } from "react-redux";
+
 const FavCities = () => {
+  const favList = useSelector((state) => state.city.cities);
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
+  const dispatch = useDispatch();
+  const [value, setValue] = React.useState(0);
 
-    const favList = useSelector((state)=>state.city.cities);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const darkTheme = useSelector((state) => state.theme.darkTheme);
-    const [value, setValue] = React.useState(0);
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  let themeAdditionBg = "";
 
-    let themeAdditionBg = '';
+  if (darkTheme) {
+    themeAdditionBg = "darkBg";
+  }
 
-    if (darkTheme){
-        themeAdditionBg = 'darkBg';
+  const tabThemeHandler = () => {
+    if (darkTheme) {
+      return { background: "#0D1117" };
     }
+  };
 
-    const tabThemeHandler = () => {
-        
-        if(darkTheme){
-            return({background: "#0D1117"});
-        }
+  useEffect(() => {
+    dispatch(startCity());
+  }, [dispatch]);
+
+  const listCheck = () => {
+    if (favList) {
+      return favList.map((item) => {
+        console.log(item);
+        return (
+          <Tab
+            key={uuidv4()}
+            label={
+              <Link to={`/cities/${item}`} style={{ textDecoration: "none" }}>
+                {item}
+              </Link>
+            }
+          ></Tab>
+        );
+      });
     }
+  };
 
-    return(
-        <div className="favCities">
-            <Box sx={{ maxWidth: { xs: '100%', sm: 1200 }, maxHeight:'150px', display:'flex' }}>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    variant="scrollable"
-                    scrollButtons
-                    allowScrollButtonsMobile
-                    aria-label="scrollable force tabs example"
-                    className={` ${themeAdditionBg}`}
-                    style={tabThemeHandler()}
-                >
-
-                    {/* <div className={`tenDays ${themeAdditionBg}`}> */}
-                                {favList.map((item)=>{
-                                    console.log(item)
-                                        return(
-                                                <Tab key={uuidv4()} label={<Link to={`/cities/${item}`} style={{ textDecoration: 'none' }}>{item}</Link>}></Tab>
-                                        )
-                                    })}   
-                        {/* </div>   */}
-                </Tabs>
-            </Box>    
-        </div>
-        
-    )
-}
+  return (
+    <div className="favCities">
+      <Box
+        sx={{
+          maxWidth: { xs: "100%", sm: 1200 },
+          maxHeight: "150px",
+          display: "flex",
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons
+          allowScrollButtonsMobile
+          aria-label="scrollable force tabs example"
+          className={` ${themeAdditionBg}`}
+          style={tabThemeHandler()}
+        >
+          {listCheck()}
+        </Tabs>
+      </Box>
+    </div>
+  );
+};
 
 export default FavCities;
